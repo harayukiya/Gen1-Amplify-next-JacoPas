0eaadf5327f6d51e14881d30fab94750b980f902
abic-yhara <harayukiya@nttdata.abic.co.jp>
2025-06-25 14:00:49 +0900

    add mutation

---
+++ src/graphql/mutations.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const sendEmail = /* GraphQL */ `
+  mutation SendEmail($input: EmailInput!) {
+    sendEmail(input: $input)
+  }
+`;
